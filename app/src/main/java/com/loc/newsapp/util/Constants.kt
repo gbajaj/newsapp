0e67948ff244fb733932f1d06a73b801c3ad9d80
Gaurav Bajaj <letsdecode@gmail.com>
2025-03-14 00:29:35 -0700

    home screen get news added to loaded to pages

--- app/src/main/java/com/loc/newsapp/util/Constants.kt
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
     const val USER_SETTINGS: String = "userSettings"
 
     const val APP_ENTRY: String = "appEntry"
+
+    const val API_KEY: String = "708b830b7da642909c8faad9da1fd5d2"
 }

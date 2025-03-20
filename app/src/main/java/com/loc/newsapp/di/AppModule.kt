921199be876b7f46e8f55f7e2739b5feb87d91e4
Gaurav Bajaj <letsdecode@gmail.com>
2025-03-20 11:04:11 -0700

    Adds room database supports to the app

--- app/src/main/java/com/loc/newsapp/di/AppModule.kt
+++ app/src/main/java/com/loc/newsapp/di/AppModule.kt
@@ -1,6 +1,10 @@
 package com.loc.newsapp.di
 
 import android.app.Application
+import androidx.room.Room
+import com.loc.newsapp.data.local.NewsDao
+import com.loc.newsapp.data.local.NewsDatabase
+import com.loc.newsapp.data.local.NewsTypeConvertor
 import com.loc.newsapp.data.manager.LocalUserManagerImpl
 import com.loc.newsapp.data.remote.NewsApi
 import com.loc.newsapp.data.repository.NewsRepositoryImpl
@@ -13,6 +17,7 @@ import com.loc.newsapp.domain.usecases.app_entry.SaveAppEntry
 import com.loc.newsapp.domain.usecases.news.GetNews
 import com.loc.newsapp.domain.usecases.news.SearchNews
 import com.loc.newsapp.util.Constants.BASE_URL
+import com.loc.newsapp.util.Constants.NEWS_DATABASE_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -62,4 +67,19 @@ object AppModule {
             searchNews = SearchNews(newsRepository)
         )
     }
+
+    @Provides
+    @Singleton
+    fun provideNewDatabase(application: Application): NewsDatabase {
+        return Room.databaseBuilder(
+            context = application, klass = NewsDatabase::class.java,
+            name = NEWS_DATABASE_NAME
+        ).addTypeConverter(NewsTypeConvertor()).fallbackToDestructiveMigration().build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewsDao(
+        newsDatabase: NewsDatabase
+    ): NewsDao = newsDatabase.newsDao
 }
--- app/src/main/java/com/loc/newsapp/util/Constants.kt
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -8,4 +8,6 @@ object Constants {
     const val API_KEY: String = "708b830b7da642909c8faad9da1fd5d2"
 
     const val BASE_URL: String = "https://newsapi.org/v2/"
+
+    const val NEWS_DATABASE_NAME: String = "news_db"
 }
